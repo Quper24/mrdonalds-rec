fa8e25d3c89e6881fb54d25a9f9a354fb7b8b41e
Leskin Maksim <Quper2405@gmail.com>
2020-07-17 16:59:14 +0200

    ready 4

---
+++ src/Components/DBMenu.js
@@ -0,0 +1,80 @@
+const dbMenu = {
+	burger: [
+		{
+			name: 'JS Burger',
+			img: '/menu/js-burger.png',
+			id: 1,
+			price: 250
+		},
+		{
+			name: 'Borsh Burger',
+			img: '/menu/borsh-burger.png',
+			id: 2,
+			price: 499.9
+		},
+		{
+			name: 'Freckles Burger',
+			img: '/menu/freckles-burger.png',
+			id: 3,
+			price: 350
+		},
+		{
+			name: 'Timon Burger',
+			img: '/menu/black-burger.png',
+			id: 4,
+			price: 150
+		},
+		{
+			name: 'React Burger',
+			img: '/menu/react-burger.png',
+			id: 5,
+			price: 299.9
+		},
+		{
+			name: 'Sun Burger',
+			img: '/menu/sun-burger.png',
+			id: 6,
+			price: 200
+		},
+	],
+	other: [
+		{
+			name: 'Coka',
+			img: '/menu/soda.png',
+			id: 1,
+			price: 50
+		},
+		{
+			name: 'Чайкофф',
+			img: '/menu/tea.png',
+			id: 2,
+			price: 70
+		},
+		{
+			name: 'Лукошко Фри',
+			img: '/menu/french-fries.png',
+			id: 3,
+			price: 75
+		},
+		{
+			name: 'Нагиевцы',
+			img: '/menu/nuggets.png',
+			id: 4,
+			price: 100
+		},
+		{
+			name: 'Кофе',
+			img: '/menu/coffee.png',
+			id: 5,
+			price: 150
+		},
+		{
+			name: 'Гринч',
+			img: '/menu/green-coffee.png',
+			id: 6,
+			price: 175.25
+		},
+	],
+};
+
+export default dbMenu;
