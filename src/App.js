25486b85b0ef92985fc1731d9424e771018c62f7
Leskin Maksim <Quper2405@gmail.com>
2020-08-27 16:06:54 +0200

    lesson 12 ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { NavBar } from './Components/NavBar/NavBar';
 import { Menu } from './Components/Menu/Menu';
 import { GlobalStyle } from './Components/Styled/GlobalStyle';
@@ -6,9 +8,22 @@ import { ModalItem } from './Components/Modal/ModalItem';
 import { Order } from './Components/Order/Order';
 import { useOpenItem } from './Components/Hooks/useOpenItem';
 import { useOrders } from './Components/Hooks/useOrders';
+import { useAuth } from './Components/Hooks/useAuth';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBQfs7hN-JIEKld0MPV_QgcILIwJpbmRMc",
+	authDomain: "mrdonalds-b8ef4.firebaseapp.com",
+	databaseURL: "https://mrdonalds-b8ef4.firebaseio.com",
+	projectId: "mrdonalds-b8ef4",
+	storageBucket: "mrdonalds-b8ef4.appspot.com",
+	messagingSenderId: "442233330321",
+	appId: "1:442233330321:web:2fa43b79e27e942e69bd30"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-
+	const auth = useAuth(firebase.auth);
 	const openItem = useOpenItem();
 	const orders = useOrders();
 
@@ -16,7 +31,7 @@ function App() {
 	return (
 		<>
 			<GlobalStyle/>
-			<NavBar/>
+			<NavBar {...auth}/>
 			<Order {...orders} {...openItem} />
 			<Menu {...openItem}/>
 			{openItem.openItem && <ModalItem {...openItem} {...orders}/>}
