e3b8c3d8ef8639c4f31df43e06286f4bb7612add
Leskin Maksim <Quper2405@gmail.com>
2020-09-02 19:44:45 +0200

    lesson 13 ready

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -4,6 +4,8 @@ import { ButtonCheckout } from '../Styled/ButtonCheckout';
 import { OrderListItem } from './OrderListItem';
 import { totalPriceItems } from '../Functions/secondaryFunction';
 import { formatCurrency } from '../Functions/secondaryFunction';
+import { projection } from '../Functions/secondaryFunction';
+
 
 const OrderStyled = styled.section`
     position: fixed;
@@ -47,7 +49,28 @@ const EmptyList = styled.p`
     text-align: center;
 `;
 
-export const Order = ({ orders, setOrders, setOpenItem }) => {
+const rulesData = {
+	itemName: ['name'],
+	price: ['price'],
+	count: ['count'],
+	topping: ['topping', arr => arr.filter(obj => obj.checked).map(obj => obj.name),
+		arr => arr.length ? arr : 'no topping'],
+	choice: ['choice', item => item ? item : 'no choices'],
+}
+
+export const Order = ({ orders, setOrders, setOpenItem, authentication, logIn, firebaseDatabase}) => {
+	const dataBase = firebaseDatabase();
+
+
+	const sendOrder = () => {
+		const newOrder = orders.map(projection(rulesData));
+		dataBase.ref('orders').push().set({
+			nameClient: authentication.displayName,
+			email: authentication.email,
+			order: newOrder
+		});
+		
+	}
 
 	const deleteItem = index => {
 		const newOrders = orders.filter((item, i) =>
@@ -83,7 +106,13 @@ export const Order = ({ orders, setOrders, setOpenItem }) => {
 				<span>{totalCounter}</span>
 				<TotalPrice>{formatCurrency(total)}</TotalPrice>
 			</Total>
-			<ButtonCheckout>Оформить</ButtonCheckout>
+			<ButtonCheckout onClick={() => {
+				if (authentication) {
+					sendOrder();
+				} else {
+					logIn()
+				}
+			}}>Оформить</ButtonCheckout>
 		</OrderStyled>
 	)
 };
